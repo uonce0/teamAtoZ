80365d94df4ebc8dc7496ab059d5ca3a221f9f04
uonce0 <ywsong000@gmail.com>
2024-09-28 20:22:42 +0900

    initial commit

---
+++ src/test/java/pre_capstone/teamAtoZ/TeamAtoZApplicationTests.java
@@ -0,0 +1,13 @@
+package pre_capstone.teamAtoZ;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TeamAtoZApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
