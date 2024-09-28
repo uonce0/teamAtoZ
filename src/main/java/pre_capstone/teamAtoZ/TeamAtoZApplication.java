d6c52cee3c768bcc679e683f5b9aab14f92b432d
uonce0 <ywsong000@gmail.com>
2024-09-28 20:22:42 +0900

    initial commit

---
+++ src/main/java/pre_capstone/teamAtoZ/TeamAtoZApplication.java
@@ -0,0 +1,14 @@
+package pre_capstone.teamAtoZ;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+
+@SpringBootApplication(exclude= {DataSourceAutoConfiguration.class})
+public class TeamAtoZApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TeamAtoZApplication.class, args);
+	}
+
+}
