74426bd2a9db7a17805eddd9e8bc7e45485526c4
uonce0 <ywsong000@gmail.com>
2024-10-17 17:39:26 +0900

    controller-1

--- src/main/java/pre_capstone/teamAtoZ/controller/ImgGenerationController.java
+++ src/main/java/pre_capstone/teamAtoZ/controller/ImgGenerationController.java
@@ -5,9 +5,7 @@ import pre_capstone.teamAtoZ.service.GPTService;
 import pre_capstone.teamAtoZ.service.ImgGenerationService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 //@Controller
 @RestController
